c0e7c8217b5f3b5128c3881ab04ddc9d21ab9665
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-06-28 08:03:06 +0900

    モデルにバリデーションの記述をした

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' }
+
+  validates :nickname, presence: true
+  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
+  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
+  validates :birthday, presence: true
+
 end
