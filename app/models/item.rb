3758b5f0e57c0aeb06bf463c39474aba0208951a
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-07-03 08:58:14 +0900

    Itemモデルにバリデーションの記述をした。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :title, :description, :category_id, :condition_id, :shipping_fee_bearer_id, :prefecture_id, :shipping_delivery_time_id, :price, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
 end
