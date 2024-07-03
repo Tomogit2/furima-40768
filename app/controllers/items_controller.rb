86cf4ec3b667b75da14c0fe5155673de975a15fd
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-07-04 08:21:30 +0900

    ルーティングとItemsコントローラーへ'new'アクションと'create'アクションを記述した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
-  def index
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: 'Item was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :category_id, :condition_id, :shipping_fee_bearer_id, :prefecture_id, :shipping_delivery_time_id, :price).merge(user_id: current_user.id)
   end
 end
