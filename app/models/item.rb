d547aeabfb73a4d0ef5e9ebe5f5d08f97f10477a
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-07-06 16:31:58 +0900

    プルダウンメニューの設定を修正した。
    ItemモデルにActiveHashのアソシエーションを追記した。ActiveHashの各モデルに対応するデータのモデル名を大文字に修正した。

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,11 @@ class Item < ApplicationRecord
   has_one_attached :item_image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, condition, shipping_fee_bearer
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_bearer
+  belongs_to :prefecture
+  belongs_to :shipping_delivery_time
 
   validates :title, :description, :category_id, :condition_id, :shipping_fee_bearer_id, :prefecture_id, :shipping_delivery_time_id, :price, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
