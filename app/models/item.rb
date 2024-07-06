fafdbb05462191201a4dbee220e25705488e115a
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-07-06 15:53:25 +0900

    ActiveHashのアソシエーションを設定し、プルダウンメニューを記述した。
    カテゴリー、商品の状態、配送料の負担、発送元の地域、発送までの日数の4項目のプルダウンメニューを記述した。

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :item_image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, condition, shipping_fee_bearer
+
   validates :title, :description, :category_id, :condition_id, :shipping_fee_bearer_id, :prefecture_id, :shipping_delivery_time_id, :price, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_bearer_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
