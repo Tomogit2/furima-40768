2f83ce2fe27650d11035dbd84e593d7b700a6ba4
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-07-07 14:52:47 +0900

    itemモデルのファクトリを定義した。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    title { "Sample Item1" }
+    description { "This is a sample item1." }    
+
+    category_id { 2 }
+    category_id { 2 }
+    condition_id { 2 }
+    shipping_fee_bearer_id { 2 }
+    prefecture_id { 2 }
+    shipping_delivery_time_id { 2 }
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
+    association :user
   end
 end
+
+
+
