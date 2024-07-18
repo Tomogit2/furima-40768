982317dc51813263a5a44b40e9d680d3d59d14e0
Tomogit2 <tomotomotomotomo_@hotmail.co.jp>
2024-07-19 08:55:50 +0900

    Formオブジェクト内にバリデーションを記載しているため削除した。

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,9 +1,3 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :purchase
-
-  validates :zip_code, presence: true
-  validates :prefecture_id, presence: true
-  validates :municipalities, presence: true
-  validates :street_address, presence: true
-  validates :telephone_number, presence: true
 end
